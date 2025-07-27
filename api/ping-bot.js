8776643a558bf5b102aacfeef163ec1227668c05
_ZiolKen <0022446688d@gmail.com>
2025-07-27 10:31:15 +0700

    Update ping-bot.js

--- api/ping-bot.js
+++ api/ping-bot.js
@@ -1,9 +1,16 @@
 export default async function handler(req, res) {
   try {
-    const response = await fetch('https://discord-bot-e6f7.onrender.com');
+    const response = await fetch('https://discord-bot-us.onrender.com');
     const text = await response.text();
     res.status(200).send(`✅ Pinged bot. Got response: ${text}`);
   } catch (err) {
     res.status(500).send(`❌ Failed to ping bot: ${err.message}`);
+  }
+  try {
+    const response = await fetch('https://discord-bot-us.onrender.com/status');
+    const text = await response.text();
+    res.status(200).send(`✅ Pinged status. Got response: ${text}`);
+  } catch (err) {
+    res.status(500).send(`❌ Failed to ping status: ${err.message}`);
   }
 }
